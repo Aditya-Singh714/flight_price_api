1b664c712349cd7dbe8032eb552390651492d64e
Aditya-Singh714 <adityajaat0000@gmail.com>
2025-03-05 23:18:35 +0530

    add a route

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,29 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Root Route
+app.get('/', (req, res) => {
+    res.json({
+        message: "Welcome to the Flight Price API",
+        routes: {
+            getFlights: {
+                method: "GET",
+                endpoint: "/api/flights?source=USA&destination=India&date=2021-12-31",
+            },
+            addFlight: {
+                method: "POST",
+                endpoint: "/api/flights",
+                description: "Adds a new flight with pricing details.",
+                bodyExample: {
+                    source:"USA",
+                    destination:"India",
+                    date: "2021-12-31",
+                }
+            }
+        }
+    });
+});
+
 // Routes
 app.use('/api/flights', flightRoute);
 
@@ -21,8 +44,8 @@ app.use('/api/flights', flightRoute);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(PORT, async() => {
+app.listen(PORT, async () => {
     console.log(`Flight Price API is running on port http://localhost:${PORT}`);
-
+    
     await connectToDatabase();
 });
